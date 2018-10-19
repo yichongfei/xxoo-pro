ed1593bf75374f45faca7d1a0b41cd2c2f5bf02c
Administrator <Administrator@PC201801220858>
2018-10-19 13:17:02 +0800

    123

--- xxoo-pro/src/main/java/com/xxss/SpringContext.java
+++ xxoo-pro/src/main/java/com/xxss/SpringContext.java
@@ -60,7 +60,7 @@ public class SpringContext implements ApplicationContextAware {
 		  AmazonS3Object S3 = new AmazonS3Object();
 		  
 		
-	      S3.saveVideo2DB(VideoService,"2018-09-15");
+		  S3.saveVideo2DB(VideoService,"2018-10-19-01");
 		
 	      
 		 
--- xxoo-pro/src/main/java/com/xxss/config/AccountConfig.java
+++ xxoo-pro/src/main/java/com/xxss/config/AccountConfig.java
@@ -2,9 +2,9 @@ package com.xxss.config;
 
 public class AccountConfig {
 	
-	public static final String  PHOTO_PATH = "F:\\photopath\\";
+	public static final String  PHOTO_PATH = "C:\\photopath\\";
 	
-	public static final String  S3PATH ="https://s3-ap-southeast-1.amazonaws.com/talent-xinjiapo/";
+	public static final String  S3PATH ="https://talent-xinjiapo.s3.amazonaws.com/";
 	
 	
 	public static final String MANGER = "star@star.com";
